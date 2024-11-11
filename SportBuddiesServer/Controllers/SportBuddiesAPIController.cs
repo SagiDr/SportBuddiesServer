837c45cd4c29f792e49cb0c127cef6122de9c444
User <User@LAB3-3>
2024-11-11 14:23:26 +0200

    Fixed the upload image problem but not in the db. need to fix that.

--- SportBuddiesServer/Controllers/SportBuddiesAPIController.cs
+++ SportBuddiesServer/Controllers/SportBuddiesAPIController.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
 namespace SportBuddiesServer.Controllers
 {
-
-
     [Route("api")]
     [ApiController]
     public class SportBuddiesAPIController : ControllerBase
@@ -75,6 +73,100 @@ namespace SportBuddiesServer.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPost("UploadProfileImage")]
+        public async Task<IActionResult> UploadProfileImageAsync(IFormFile file)
+        {
+            //Check if who is logged in
+            string? userEmail = HttpContext.Session.GetString("loggedInUser");
+            if (string.IsNullOrEmpty(userEmail))
+            {
+                return Unauthorized("User is not logged in");
+            }
+
+            //Get model user class from DB with matching email. 
+            Models.User? user = context.GetUser(userEmail);
+            //Clear the tracking of all objects to avoid double tracking
+            context.ChangeTracker.Clear();
+
+            if (user == null)
+            {
+                return Unauthorized("User is not found in the database");
+            }
+
+
+            //Read all files sent
+            long imagesSize = 0;
+
+            if (file.Length > 0)
+            {
+                //Check the file extention!
+                string[] allowedExtentions = { ".png", ".jpg" };
+                string extention = "";
+                if (file.FileName.LastIndexOf(".") > 0)
+                {
+                    extention = file.FileName.Substring(file.FileName.LastIndexOf(".")).ToLower();
+                }
+                if (!allowedExtentions.Where(e => e == extention).Any())
+                {
+                    //Extention is not supported
+                    return BadRequest("File sent with non supported extention");
+                }
+
+                //Build path in the web root (better to a specific folder under the web root
+                string filePath = $"{this.webHostEnvironment.WebRootPath}\\profileImages\\{user.UserId}{extention}";
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+
+                    if (IsImage(stream))
+                    {
+                        imagesSize += stream.Length;
+                    }
+                    else
+                    {
+                        //Delete the file if it is not supported!
+                        System.IO.File.Delete(filePath);
+                    }
+
+                }
+
+            }
+
+            DTO.User dtoUser = new DTO.User(user);
+            dtoUser.ProfileImageExtention = GetProfileImageVirtualPath(dtoUser.UserId);
+            return Ok(dtoUser);
+        }
+
+        //Helper functions
+
+        //this function gets a file stream and check if it is an image
+        private static bool IsImage(Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+
+            List<string> jpg = new List<string> { "FF", "D8" };
+            List<string> bmp = new List<string> { "42", "4D" };
+            List<string> gif = new List<string> { "47", "49", "46" };
+            List<string> png = new List<string> { "89", "50", "4E", "47", "0D", "0A", "1A", "0A" };
+            List<List<string>> imgTypes = new List<List<string>> { jpg, bmp, gif, png };
+
+            List<string> bytesIterated = new List<string>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                string bit = stream.ReadByte().ToString("X2");
+                bytesIterated.Add(bit);
+
+                bool isImage = imgTypes.Any(img => !img.Except(bytesIterated).Any());
+                if (isImage)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         //this function check which profile image exist and return the virtual path of it.
         //if it does not exist it returns the default profile image virtual path
         private string GetProfileImageVirtualPath(int userId)
@@ -99,14 +191,6 @@ namespace SportBuddiesServer.Controllers
             }
 
             return virtualPath;
-        }
-
-
-        [HttpGet]
-        [Route("TestServer")]
-        public ActionResult<string> TestServer()
-        {
-            return Ok("Server Responded Successfully");
         }
     }
 }
